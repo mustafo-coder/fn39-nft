916d573ff712b4e81769b2afba72cb303fd1e5e9
Asadbek Tursunaliyev <madrahimoffasadbek@gmail.com>
2025-05-30 10:29:12 +0500

    Creators

--- src/components/Creators.jsx
+++ src/components/Creators.jsx
@@ -2,7 +2,142 @@ import React from 'react'
 
 const Creators = () => {
   return (
-    <div>Creators</div>
+    <div className='container py-10  text-white min-h-screen bg-[#2B2B2B]'>
+        <div className='flex justify-between'>
+          <div className=''>
+              <h1 className='text-2xl font-bold'>Top Creators</h1>
+              <p className='font-thin text-white/90 mt-2'>Checkout Top Rated Creators on the NFT Marketplace</p>
+          </div>
+          <div className=''>
+            <a href="/" className='py-2 px-5 hover:bg-primary hover:*:text-[#2B2B2B] transition-all border-primary border-2 rounded-2xl inline-block'>
+            <i class="fa-solid fa-rocket mr-2 text-primary"></i>
+            View Rankings</a>
+          </div>
+        </div>
+
+        <div className='grid-cols-2 grid gap-10 md:grid-cols-4'>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>1</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>2</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>3</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>4</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>5</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>6</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>7</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>8</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>9</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>10</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>11</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+          <div className='bg-[#3B3B3B] relative flex  md:py-10 md:inline-block  rounded-2xl '>
+            <div>
+              <p className='bg-[#2B2B2B] p-2 py-0.5 top-1 left-1 md:p-4 absolute md:top-3 md:left-3 text-[14px] md:text-[18px] md:py-2 rounded-full text-[#858584]'>12</p>
+              <img src="/avatar.png " alt="avatar1" className='mx-auto max-w-[100px]' />
+            </div>
+            <div className=''>
+              <h1 className='text-[18px] text-center pt-3'>Keepitreal</h1>
+              <p className='text-white/50 text-[14px] pt-2 pb-5 text-center'>Total Sales: <span className='text-white'>34.53 ETH</span></p>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
